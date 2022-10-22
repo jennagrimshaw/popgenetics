94834ba78a154f7488c0a160d62165a70c7e3a09
Jenna Grimshaw <Jenna.grimshaw@ttu.edu>
2022-10-21 21:25:18 -0500

    Update 2B-popmap.sh

--- Step 2/2B-popmap.sh
+++ Step 2/2B-popmap.sh
@@ -1,1 +1,14 @@
 
+DIR=<directory>
+
+cd $DIR
+
+INFO="<directory>ist.txt"
+
+echo $INFO
+
+while IFS= read -r line; do
+    cut -c -9 >> temp-popmap.txt
+done < $INFO
+
+uniq temp-popmap.txt popmap.txt
